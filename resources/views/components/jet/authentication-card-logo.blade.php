eec6fdac15e5d41f2436bdf54a953226ec18d271
kalai <kalaiararasan200229@gmail.com>
2024-03-18 16:40:08 +0530

    #24 working Aaran logo

--- resources/views/components/jet/authentication-card-logo.blade.php
+++ resources/views/components/jet/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/">
 {{--    <x-icons.icons :icons="'whatsapp'"  class="block w-16 h-auto  grid-rows-1 mr-3 mb-5"/>--}}
-    <img class="w-24 h-24 mr-auto mt-10 ml-auto" src="../../../../images/logo.png" alt="alternative" />
+    <img class="w-36 h-36 mr-auto mt-10 ml-auto" src="../../../../images/logo.png" alt="alternative" />
 </a>

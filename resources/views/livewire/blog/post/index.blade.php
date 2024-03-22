e26153d64c1cda8d8dc74ecc0900c6c87643aced
Gowtham <gowtham@aaran.org>
2024-03-22 13:04:17 +0530

    #38 aaran office copy

--- resources/views/livewire/blog/post/index.blade.php
+++ resources/views/livewire/blog/post/index.blade.php
@@ -6,14 +6,14 @@
     @foreach($list as $index => $row)
 
         <a href="{{route('posts.upsert',[$row->id])}}">
-        <div class="flex flex-col border-b border-amber-400 gap-2 justify-center py-3">
+        <div class="flex flex-col border-b border-gray-400 gap-2 justify-center items-center py-3 ">
             <div class="flex">
-               <div class="flex-shrink-0 h-20 w-20 mr-10" >
-                   <img src="{{ \Illuminate\Support\Facades\Storage::url($row->image) }}"/>
+               <div class="flex-shrink-0 h-52 w-52 mr-10 rounded-lg " >
+                   <img src="{{ \Illuminate\Support\Facades\Storage::url($row->image) }}" class="rounded-2xl w-52 h-52"/>
                 </div>
-            <div >
-            <div class="text-xl font-semibold">{{$index+1}}&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;{{$row->title}}</div>
-            <div class="text-sm text-gray-500 font-semibold italic line-clamp-2">{{$row->body}}</div>
+            <div class="pl-3">
+            <div class="text-xl font-extrabold">{{$index+1}}&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;{{$row->title}}</div>
+            <div class="text-sm text-gray-500 font-semibold italic line-clamp-2 mt-2 text-wrap ">{{$row->body}}</div>
             </div>
             </div>
         </div>

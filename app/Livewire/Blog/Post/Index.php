1d53ed2dedd3be703214bcf444fcb3c2a28017fb
Divya <divyadharshini146@gmail.com>
2024-04-01 12:16:46 +0530

    #83 blog post,rework on index file

--- app/Livewire/Blog/Post/Index.php
+++ app/Livewire/Blog/Post/Index.php
@@ -5,10 +5,18 @@ namespace App\Livewire\Blog\Post;
 use App\Models\Blog\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $user_id;
+    public function mount()
+    {
+        if (Auth::user()!=''){
+            $this->user_id=Auth::user()->id;
+        }
+    }
     public function create(): void
     {
         $this->redirect(route('posts.upsert', ['0']));
@@ -17,7 +25,11 @@ class Index extends Component
     public function render()
     {
         return view('livewire.blog.post.index')->layout('layouts.web')->with([
-            'list' => Post::all(),'list1'=>Post::find(1)
+            'list' => Post::all()
+            ->when($this->user_id,function ($query,$user_id) {
+                return $query->where('user_id', '=', $user_id);
+            })
         ]);
+
     }
 }

18d7aa0fc45ee1a72d507fd75054c695e75d2677
Divya <divyadharshini146@gmail.com>
2024-04-02 19:09:51 +0530

    #89 rework on blog

--- resources/views/livewire/blog/post/upsert.blade.php
+++ resources/views/livewire/blog/post/upsert.blade.php
@@ -27,6 +27,7 @@
                 @else
                     <img src="{{ \Illuminate\Support\Facades\Storage::url($image)}}" alt="{{$image}} "
                          class=" mt-8 mb-8 h-80 w-full rounded-lg pb-4"/>
+
                 @endif
             </div>
 
@@ -43,14 +44,16 @@
             </div>
 
             <div>
-                <button wire:click="set_delete({{$id}})" wire:confirm="Are you sure you want to delete this ?" class='inline-flex items-center px-2 py-2 border border-transparent
+                @if($id!=0)
+                    <button wire:click="set_delete({{$id}})" wire:confirm="Are you sure you want to delete this ?"
+                            class='inline-flex items-center px-2 py-2 border border-transparent
                                rounded-md font-semibold text-xs text-white uppercase tracking-widest
                                focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150
                                focus:ring-red-500 bg-red-600 hover:bg-red-500 active:bg-red-700 border-red-600'>&nbsp;
-                    <x-icons.icon :icon="'trash'"
-                                  class="h-5 w-auto block px-1.5"/>&nbsp;Delete
-                </button>
-            </div>
+                        <x-icons.icon :icon="'trash'"
+                                      class="h-5 w-auto block px-1.5"/>&nbsp;Delete
+                    </button>
+                @endif</div>
         </div>
     </div>
 </div>

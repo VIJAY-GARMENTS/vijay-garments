ebb031c8a6890f8814f4c48cfdc5a10f73508fd2
“SUNDAR-Office” <aaranoffice@gmail.com>
2024-04-10 19:59:44 +0530

    #2 refactor menu

--- resources/views/components/menu/items/entries-menu.blade.php
+++ resources/views/components/menu/items/entries-menu.blade.php
@@ -1,5 +1,4 @@
 <x-menu.base.li-menuitem :routes="'sales'" :label="'Sales'"/>
-<x-menu.base.li-menuitem :routes="'sales.reports'" :label="'Sales Report'"/>
 <x-menu.base.li-menuitem :routes="'purchases'" :label="'Purchase'"/>
 <x-menu.base.li-menuitem :routes="'receipts'" :label="'Receipt'"/>
 <x-menu.base.li-menuitem :routes="'payments'" :label="'Payment'"/>

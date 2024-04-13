65527d824ce3f8517cc75057ded43f3666c399ba
Gowtham <gowtham@aaran.org>
2024-04-13 11:02:21 +0530

    #123  menu correction

--- resources/views/components/menu/items/admin-menu.blade.php
+++ resources/views/components/menu/items/admin-menu.blade.php
@@ -1,17 +1,12 @@
-@editor
 <x-menu.base.li-menuitem :routes="'fees'" :label="'Client Fee'"/>
 <x-menu.base.li-menuitem :routes="'bankBalances'" :label="'Balance'"/>
 <x-menu.base.li-menuitem :routes="'payment-slips'" :label="'Payment Slip'"/>
 <x-menu.base.li-menuitem :routes="'banks'" :label="'Client Banks'"/>
-@endeditor
 
-@admin
+
 <x-menu.base.li-menuitem :routes="'socials'" :label="'Social Ids'"/>
 
-@endadmin
 
-@supervisor
 <x-menu.base.li-menuitem :routes="'creditbooks'" :label="'Credit Book'"/>
 <x-menu.base.li-menuitem :routes="'allTasks'" :label="'All Task'"/>
 <x-menu.base.li-menuitem :routes="'activities.admins'" :label="'All Activities'"/>
-@endsupervisor
